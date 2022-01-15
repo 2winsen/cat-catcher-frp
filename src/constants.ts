200679c8989ff3519cc802aaf7ca376cec1116dc
vsakels <vsakels@evolution.com>
2022-01-15 15:38:40 +0200

    game difficulty

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const BOARD_WIDTH = 8;
 export const BOARD_HEIGHT = 10;
 export const WINDOWS_COUNT = 8;
-export const GAME_INTERVAL = 1000;
+export const GAME_INTERVAL = 800;
 export const CANVAS_CELL_SIZE = 50;
